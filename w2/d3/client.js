5303c12685302f362d8592b1647116e49ddf7535
Mariya <va.mariya@yahoo.com>
2021-11-06 20:55:05 +0000

    exported connect function

---
+++ w2/d3/client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",  // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connected");// code that does something when the connection is first established
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+// module.exports = {
+//  host,
+//  port, 
+// };
+
+module.exports = {
+  connect, 
+ };
--- w2/d3/play.js
+++ w2/d3/play.js
@@ -1,22 +1,25 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",  // IP address here,
-    port: 50541// PORT number here,
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",  // IP address here,
+//     port: 50541// PORT number here,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connected");// code that does something when the connection is first established
-  });
-  return conn;
-};
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+//   conn.on("connect", () => {
+//     console.log("Connected");// code that does something when the connection is first established
+//   });
+//   return conn;
+// };
 
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
+// const { host, port} = require("./client.js");
+const { connect } = require("./client.js");
+
 
 
 
