6ee904902d8cc3bcccecf93ac089cb4c27b442b5
Mariya <va.mariya@yahoo.com>
2021-11-06 19:01:31 +0000

    pushing snek, connected

---
+++ w2/d3/play.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",  // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connected");// code that does something when the connection is first established
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+
+
+
