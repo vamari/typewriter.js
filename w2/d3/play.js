a73d496df134d6ab30d96fbcf64568c8b334d9c3
Mariya <va.mariya@yahoo.com>
2021-11-07 12:46:40 +0000

    set setupInput,handleUserInput to client.js

--- w2/d3/play.js
+++ w2/d3/play.js
@@ -21,7 +21,22 @@
 
 const { connect } = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+ 
+  const handleUserInput = function () {
+    stdin.on("data", handleUserInput);// your code here
+  };
+  
 
+  return stdin;
+};
+
+setupInput();
 
 
 
